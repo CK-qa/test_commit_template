a883c58d2d0a948d20d456fe8a10f897572521b7
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-05-14 15:03:58 +0300

    #global template with space [x]clear initial commit message

--- src/main/java/pack1/Test4.java
+++ src/main/java/pack1/Test4.java
@@ -8,6 +8,7 @@ public class Test4 {
     //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
     //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
     //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
+    //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
     //
     //
     //
