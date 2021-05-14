2d232e5f626d0ac50c6a3c5616e5bc440c160806
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-05-14 13:22:01 +0300

    #template with tab [v]clear initial commit message

--- src/main/java/pack1/Test4.java
+++ src/main/java/pack1/Test4.java
@@ -3,6 +3,7 @@ package pack1;
 public class Test4 {
 
     //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
+    //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
     //
     //
 }
