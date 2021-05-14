ffc757085d3b562bc245b4ca8d59b86e9207791c
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-05-14 13:45:14 +0300

    한국어
        [x]clear initial commit message

--- src/main/java/pack1/Test4.java
+++ src/main/java/pack1/Test4.java
@@ -5,6 +5,7 @@ public class Test4 {
     //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
     //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
     //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
+    //C:\Users\viktoria.bozhko\IdeaProjects\test_commit_template\child.txt
     //
     //
     //
